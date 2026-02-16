f87bff67cc9ef648f17e1d637751a8ebcb23ca14
Roberto Villegas-Diaz <villegar@users.noreply.github.com>
2026-02-16 15:27:49 +0000

    Reformat code with 'air'

--- source/render_docs.R
+++ source/render_docs.R
@@ -6,7 +6,7 @@
 # July 2025
 # SETUP ----
 args <- commandArgs(trailingOnly = TRUE) # read CL arguments
-# 1st argument: INPUT_DIR 
+# 1st argument: INPUT_DIR
 if (length(args) >= 1) {
   INPUT_DIR <- args[1]
 } else {
@@ -25,10 +25,15 @@ if (length(args) >= 3) {
   CLEAR_DOCS <- FALSE
 }
 
-message("Using the following configuration:",
-        "\n  INPUT_DIR: ", INPUT_DIR,
-        "\n  OUTPUT_DIR: ", OUTPUT_DIR,
-        "\n  CLEAR_DOCS: ", CLEAR_DOCS)
+message(
+  "Using the following configuration:",
+  "\n  INPUT_DIR: ",
+  INPUT_DIR,
+  "\n  OUTPUT_DIR: ",
+  OUTPUT_DIR,
+  "\n  CLEAR_DOCS: ",
+  CLEAR_DOCS
+)
 
 # create directory in case an empty directory is received
 dir.create(INPUT_DIR, recursive = TRUE)
@@ -41,7 +46,7 @@ clean_url <- function(url, input_dir) {
     stringr::str_remove_all(stringr::str_escape(input_dir)) |>
     stringr::str_remove_all("^[\\/]*")
 }
- 
+
 # GENERATE HTML ----
 header_html <- glue::glue(
   "---
@@ -53,7 +58,8 @@ title: DataSHIELD: Latest Test Status
 <p>DataSHIELD is an infrastructure and series of R packages that enables the remote and non-disclosive analysis of sensitive research data. DataSHIELD has been used with real world and consented research data for over 10 years.</p>
 <p>The following are auto-generated reports for testing key DataSHIELD R packages.</p>
 <p><bf>Last updated:</bf> {Sys.Date()} @ {format(Sys.time(), '%H:%M:%S')}
-\n")
+\n"
+)
 
 # list directories inside INPUT_DIR
 dirs_lst <- INPUT_DIR |>
@@ -76,15 +82,25 @@ body_html <- dirs_lst[idx] |>
         list.dirs(full.names = TRUE, recursive = FALSE)
       # detect directories that are not the latest version, avoid cluttering repo
       idx <- stringr::str_detect(list_all_dirs_level_2, "latest", negate = TRUE)
-      message("Deleting unwanted directories: \n", 
-              paste0("- ", list_all_dirs_level_2[idx], collapse = "\n"))
+      message(
+        "Deleting unwanted directories: \n",
+        paste0("- ", list_all_dirs_level_2[idx], collapse = "\n")
+      )
       # delete unwanted directories
       unlink(list_all_dirs_level_2[idx], force = TRUE, recursive = TRUE)
     }
-    
+
     aux <- d |>
       list.dirs(full.names = TRUE, recursive = FALSE) |>
-      purrr::map(\(sd) paste0("\t<li><a href='", clean_url(sd, INPUT_DIR), "/latest'>", basename(sd) ,"</a></li>")) |>
+      purrr::map(\(sd) {
+        paste0(
+          "\t<li><a href='",
+          clean_url(sd, INPUT_DIR),
+          "/latest'>",
+          basename(sd),
+          "</a></li>"
+        )
+      }) |>
       paste0(collapse = "\n")
     new_section <- paste0(new_section, aux, "\n</ul>\n\n")
   }) |>
@@ -98,5 +114,7 @@ paste0(header_html, body_html, collapse = "") |>
   readr::write_lines(file.path(OUTPUT_DIR, "index.html"))
 
 # list files in the current directory
-message("\nFiles that will be deployed:\n",
-        paste0("- ", list.files(INPUT_DIR, recursive = TRUE), collapse = "\n"))
+message(
+  "\nFiles that will be deployed:\n",
+  paste0("- ", list.files(INPUT_DIR, recursive = TRUE), collapse = "\n")
+)
